4fcc471989b183fdfa57fd68f824c285bd5abc33
Sylvia <sylviad1221@gmail.com>
2017-01-27 01:13:33 -0500

    add landing page and styling

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var bodyParser = require("body-parser"),
     flash = require("connect-flash"),
     passport = require("passport"),
     localStrategy = require("passport-local"),
-    methodOverride = require("method-oerride"),
+    methodOverride = require("method-override");
     // Cafe = require("models/cafe"),
     // Comment = require("models/comment"),
     // User = require("models/user"),
@@ -14,26 +14,27 @@ var bodyParser = require("body-parser"),
 // ROUTES
 // var commentRoutes = require("./routes/comments"),
 //     cafeRoutes = require("./routes/cafe"),
-//     indexRoutes = require("./routes/index")
+var     indexRoutes = require("./routes/index");
 
-mongoose.connect("mongodb://localhost/cafehub");
-app.use(bodyParser.urlencoded({extende: true}));
+mongoose.connect("mongodb://localhost/cafe_hub");
+app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
 app.use(methodOverride("_method"));
 app.use(flash());
 
-app.use(require("express-session")({
-  secret: "Dream, Beleive, Discover",
-  resave: false,
-  caveUninitialized: false
-}));
+// app.use(require("express-session")({
+//   secret: "Dream, Beleive, Discover",
+//   resave: false,
+//   saveUninitialized: false
+// }));
+// app.use(passport.initialize());
+// app.use(passport.session());
+// passport.use(new localStrategy(User.authenticate()));
+// passport.serializeUser(User.serializeUser());
+// passport.deserializeUser(User.deserializeUser());
 
-app.use(passport.initialize());
-app.use(passport.session());
-passport.use(new localStrategy(User.authenticate()));
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
+app.use("/", indexRoutes);
 
 app.listen(3000, function(){
   console.log("Go to 3000!");
